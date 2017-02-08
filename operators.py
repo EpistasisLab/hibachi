b7bbc4a76aba7257b77e9a1310f85cce59a54523
Pete Schmitt <peter.r.schmitt@icloud.com>
2017-02-08 14:16:53 -0500

    operators to be used by primitives
    using internal factorial() for impute and choose functions
    hib.py no longer using maximum, minimum or ABS functions

--- operators.py
+++ operators.py
@@ -16,7 +16,7 @@
 #       COMPANY:  University of Pennsylvania
 #       VERSION:  0.1.1
 #       CREATED:  09/29/2016 10:39:21 EDT
-#      REVISION:  Wed Feb  1 11:23:08 EST 2017
+#      REVISION:  Tue Feb  7 13:09:23 EST 2017
 #===========================================================================
 
 import numpy as np
@@ -106,15 +106,14 @@ def bitxor(a,b):
     return int(a) ^ int(b)
 ######################## UNARY OPERATORS ###############################
 def ABS(a):
+    """ UNUSED: return absolute value """
     return op.abs(a)
 #----------------------------------#
 def factorial(a):
     """ returns 0 if a >= 100 """
     a = abs(round(a))
-    if(a < 100):
-        return math.factorial(a)
-    else:
-        return 0
+    a = min(a, 100)
+    return math.factorial(a)
 #----------------------------------#
 def NOT(a):
     """ if a eq 0 return 1
@@ -139,6 +138,7 @@ def logEofA(a):
 def power(a,b):
     """ return a to the power of b """
     a = abs(a)  # ensure the denial of complex number creation
+    b = min(b,100)
     try:
         z = a ** b
     except:
@@ -157,10 +157,8 @@ def permute(a,b):
     b = abs(round(b))
     if(b > a):
         a, b = b, a
-    if(a < 100):
-        return math.factorial(a) / math.factorial(a-b)
-    else:
-        return 0
+
+    return factorial(a) / factorial(a-b)
 #----------------------------------#
 def choose(a,b):
     """ n Choose r function """
@@ -168,10 +166,8 @@ def choose(a,b):
     b = op.abs(round(b))
     if(b > a):
         a, b = b, a
-    if(a < 100):
-        return math.factorial(a) / (math.factorial(b) * math.factorial(a-b))
-    else:
-        return 0
+
+    return factorial(a) / (factorial(b) * factorial(a-b))
 #----------------------------------#
 def constrainForLog(value):
     """ used by log methods """
@@ -183,20 +179,18 @@ def constrainForLog(value):
         return value
 ######################### MISC OPERATORS ###############################
 def minimum(a,b):
-    """ return the smallest value of a and b """
+    """ UNUSED: return the smallest value of a and b """
     if(a < b):
         return a
     else:
         return b
-#return min(float(a),float(b)) 
 #----------------------------------#
 def maximum(a,b):
-    """ return the largest value of a and b """
+    """ UNUSED: return the largest value of a and b """
     if(a > b):
         return a
     else:
         return b
-#return max(a,b) 
 #----------------------------------#
 def left(a, b):
     """ return left value """
